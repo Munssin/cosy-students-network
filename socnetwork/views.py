83927ba54b94320efb62b32f2eb3957028e8e654
macksik <68849259+macksik@users.noreply.github.com>
2023-01-06 19:33:51 +0200

    Add files via upload

--- socnetwork/views.py
+++ socnetwork/views.py
@@ -1,9 +1,48 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def index(request):
-    return render(request, "header/header.html")
+@login_required(login_url='login')
+def HomePage(request):
+    return render(request, "login/index.html")
+
+@csrf_exempt
+def SignupPage(request):
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+
+        if pass1!=pass2 :
+            return HttpResponse("Your password and conform password are not Same!")
+        else:
+            new_user=User.objects.create_user(uname,email,pass1)
+            new_user.save()
+            return redirect('login')
+        
+      
+
+    return render(request, "login/signup.html")
 
 
+def LoginPage(request):
+    if request.method=='POST': 
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username=username,password=pass1)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else: 
+            return HttpResponse("Username or password is incorrect!")
+
+    return render(request, "login/login.html")
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
