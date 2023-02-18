c4a8605d0f50889c6f0eccf9bf238853f5abd06d
Munssin <munssin1488@gmail.com>
2023-02-18 18:22:34 +0200

    new path structure

--- socnetwork/gulpfile.js
+++ socnetwork/gulpfile.js
@@ -3,57 +3,42 @@
 const {src, dest} = require("gulp")
 const gulp = require("gulp")
 const autoprefixer = require("gulp-autoprefixer");
-const cssbeautify =  require("gulp-cssbeautify");
 const removeComments =  require('gulp-strip-css-comments');
 const rigger = require("gulp-rigger");
-const include = require("gulp-include");
 const rename =  require("gulp-rename");
 const sass =  require("gulp-sass")( require("sass"));
-const cssnano =  require("gulp-cssnano");
-const uglify =  require("gulp-uglify");
 const plumber =  require("gulp-plumber");
 const panini =  require("panini");
-const imagemin =  require("gulp-imagemin");
 const del =  require("del");
 const notify = require("gulp-notify");
 const sourcemaps = require("gulp-sourcemaps");
-const concat = require("gulp-concat");
 const browserSync =  require("browser-sync").create();
-
 
 /*Path*/
 const srcPath = "src/"
-const distPath = "auth/"
 
 const path = {
       build: {
-          html: distPath,
-          css: distPath + "assets/css/" ,
-          js: distPath +"assets/js/",
-          images: distPath +"assets/images/",
-          fonts: distPath +"assets/fonts/"
+          html: "templates/auth",
+          css: "static/css/" ,
+          js: "static/js/",
       },
       src: {
           html: srcPath + "*.html",
           css: srcPath + "assets/scss/*.scss",
           js: srcPath + "assets/js/*.js",
-          images: srcPath + "assets/images/**/*.{json,jpeg,png,svg,jpg,gif,ico,webp,webmanifest,xml}",
-          fonts: srcPath +  "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}"
       },
       watch: {
           html: srcPath + "**/*.html",
           css: srcPath + "assets/scss/**/*.scss",
           js: srcPath + "assets/js/**/*.js",
-          images: srcPath + "assets/images/**/*.{json,jpeg,png,svg,jpg,gif,ico,webp,webmanifest,xml}",
-          fonts: srcPath +  "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}"
-      },
-      clean: "./" + distPath
+      }
 }
 
 function serve() {
     browserSync.init({
         server: {
-            baseDir: "./" + distPath
+            baseDir: "./" + "templates/auth"
         }
     });
 }
@@ -86,14 +71,7 @@ function css() {
         }))
         .pipe(sass())
         .pipe(autoprefixer())
-        .pipe(cssbeautify())
         .pipe(dest(path.build.css))
-        .pipe(cssnano({
-            zindex: false,
-            discardComments:{
-                removeAll: true
-            }
-        }))
         .pipe(removeComments())
         .pipe(rename({
             suffix: ".min",
@@ -117,7 +95,6 @@ function js() {
         }))
         .pipe(rigger())
         .pipe(dest(path.build.js))
-        .pipe(uglify())
         .pipe(rename({
             suffix: ".min",
             extname: ".js"
@@ -128,51 +105,19 @@ function js() {
 
 }
 
-function images() {
-    return src(path.src.images, {base: srcPath + "assets/images/"})
-        .pipe(imagemin([
-	imagemin.gifsicle({interlaced: true}),
-	imagemin.mozjpeg({quality: 80, progressive: true}),
-	imagemin.optipng({optimizationLevel: 5}),
-	imagemin.svgo({
-		plugins: [
-			{removeViewBox: true},
-			{cleanupIDs: false}
-		]
-	})
-]))
-        .pipe(dest(path.build.images))
-        .pipe(browserSync.reload({stream:true}));
-}
-
-function fonts() {
-    return src(path.src.fonts, {base: srcPath + "assets/fonts/"})
-    .pipe(browserSync.reload({stream:true}));
-
-}
-
-function clean() {
-    return del(path.clean)
-}
-
 function watchFiles() {
     gulp.watch([path.watch.html], html)
     gulp.watch([path.watch.css], css)
     gulp.watch([path.watch.js], js)
-    gulp.watch([path.watch.images], images)
-    gulp.watch([path.watch.fonts], fonts)
 }
 
-const build = gulp.series(clean, gulp.parallel(html, css, js, images, fonts))
+const build = gulp.series(gulp.parallel(html, css, js))
 const watch = gulp.parallel(build, watchFiles, serve)
 
 
 exports.html = html
 exports.css = css
 exports.js = js
-exports.images = images
-exports.fonts = fonts
-exports.clean = clean
 exports.build = build
 exports.watch = watch
 exports.default = watch
