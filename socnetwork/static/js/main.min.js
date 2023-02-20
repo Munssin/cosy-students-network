7e6bc7a83728ac5f750f109e26e3c14bf05b200f
KhrystynaGrytsiuk <krystyna.lullaby@gmail.com>
2023-02-20 22:45:11 +0200

    Fix position for some elements

---
+++ socnetwork/static/js/main.min.js
@@ -0,0 +1,17 @@
+
+function show() {
+    let div = document.getElementById('description-content');
+      if (div.style.display !== "none") {
+        div.style.display = "none";
+      } else {
+        div.style.display = "block";
+      }
+
+};
+function viewDiv(){
+  document.getElementById("button-for groups").style.display = "block";
+};
+
+function viewDi(){
+  document.getElementById("button-for group").style.display = "block";
+};
